84574ae4a33f7a88b1b6e132492f553dea394db4
Shownshaiju <shownshaiju@gmail.com>
2026-02-25 23:48:37 +0530

    Feat:Added old profile photo deletion logic and added docker ignore

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .tasks import delete_old_avatar_task
 
 class Profile(models.Model):
 
@@ -18,3 +21,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+@receiver(pre_save, sender=Profile)
+def delete_old_avatar_on_update(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    try:
+        old_profile = Profile.objects.get(pk=instance.pk)
+    except Profile.DoesNotExist:
+        return
+
+    if old_profile.avatar_image and old_profile.avatar_image != instance.avatar_image:
+        if old_profile.avatar_image.name != 'default.jpg':
+            # Dispatch the file name to the Celery worker queue (.delay)
+            # This takes less than 1 millisecond to execute
+            delete_old_avatar_task.delay(old_profile.avatar_image.name)

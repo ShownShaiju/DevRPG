c958e2ed9e06627ef9f0a8c0db28aae2777a36f2
Shownshaiju <shownshaiju@gmail.com>
2026-03-11 06:01:42 +0530

    Feat:Job Feature added properly

--- guilds/urls.py
+++ guilds/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('leave/', views.leave_guild, name='leave_guild'),
     path('create/', views.create_guild, name='create_guild'),
     path('quests/', views.quest_board, name='quest_board'),
+    
+    path('quests/create/', views.create_quest, name='create_quest'),
+    path('quests/<int:quest_id>/accept/', views.accept_quest, name='accept_quest'),
+    path('quests/<int:quest_id>/submit/', views.submit_quest, name='submit_quest'),
+    path('quests/submission/<int:submission_id>/approve/', views.approve_submission, name='approve_submission'),
+
 ]
